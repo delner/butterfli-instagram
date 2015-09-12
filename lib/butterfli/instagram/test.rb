102efbbb40ef366407c246dd665e0907552a2d8c
David Elner <david@davidelner.com>
2015-09-11 21:46:26 -0400

    Added: MediaObject fixture reader helper

--- lib/butterfli/instagram/test.rb
+++ lib/butterfli/instagram/test.rb
@@ -10,6 +10,19 @@ module Butterfli
           end
         end
       end
+      def read_media_objects_fixture(fixture_name)
+        fixture_path = File.join(Dir.pwd + '/spec/fixtures/', "#{fixture_name}.json")
+        if fixture = JSON.parse(File.read(fixture_path))
+          if fixture.is_a?(Hash)
+            return Hashie::Mash.new(fixture)
+          elsif fixture.is_a?(Array)
+            return fixture.collect { |h| h.is_a?(Hash) ? Hashie::Mash.new(h) : h }
+          end
+          return fixture
+        else
+          raise "Failed to load media object fixture file #{fixture_file}! File may be incorrectly formatted."
+        end
+      end
     end
   end
 end
